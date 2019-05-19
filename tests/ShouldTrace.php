a623991d2443940184dcbc906abe5f7de7c82db3
Pascal Baljet <pascal@pascalbaljetmedia.com>
2019-05-19 14:24:49 +0200

    Added typehints

--- tests/ShouldTrace.php
+++ tests/ShouldTrace.php
@@ -2,14 +2,17 @@
 
 namespace Protonemedia\LaravelTracer\Tests;
 
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 class ShouldTrace
 {
-    public function no()
+    public function no(Request $request, Response $response): bool
     {
         return false;
     }
 
-    public function yes()
+    public function yes(Request $request, Response $response): bool
     {
         return true;
     }
